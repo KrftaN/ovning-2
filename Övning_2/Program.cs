591051d4c856ebae0bc5b33defc38a23ff3720fc
KraftaN <63770425+KrftaN@users.noreply.github.com>
2024-04-05 22:38:06 +0200

    Add project files.

---
+++ Övning_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Övning_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.FirstName = "Malte";
+            person.LastName = "Elwhagen";
+
+            Console.WriteLine($"Namn: {person.FullName}");
+        }
+    }
+}
